43850b3ae161d5e4833b25987cded87e1bd2bc97
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-06-18 11:08:27 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+Vue.use((VueRouter))
 Vue.config.productionTip = false
 
+import routeLogin from "./components/components_pages/component_login"
+import routeDash from "./components/components_pages/component_dashboard"
+
+const routes = [
+    {path: '/login', component: routeLogin},
+    {path: '/dashboard', component: routeDash}
+]
+
+const router = new VueRouter({
+    routes
+})
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    router,
+    render: h => h(App)
+})
